f50920e12a183b448e7c04e3e873d146c8a664c1
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-10-14 17:01:33 +1030

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,23 +8,68 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+      mode: 'development',
+      entry: {
+          main: './src/js/index.js',
+          install: './src/js/install.js'
+      },
+      output: {
+          filename: '[name].bundle.js',
+          path: path.resolve(__dirname, 'dist'),
+      },
+      plugins: [
+          // Webpack plugin that generates our html file and injects our bundles. 
+          new HtmlWebpackPlugin({
+              template: './index.html',
+              title: 'PWA Text Editor'
+          }),
 
-    module: {
-      rules: [
-        
+          // Injects our custom service worker
+          new InjectManifest({
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js',
+          }),
+
+          new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: 'PWA Text Editor',
+              short_name: 'Txt Editor',
+              description: 'An application to document your thoughts',
+              background_color: '#225ca3',
+              theme_color: '#225ca3',
+              start_url: '/',
+              publicPath: '/',
+              icons: [{
+                  src: path.resolve('src/images/logo.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons'),
+              }, ],
+          }),
+
+
+
       ],
-    },
+
+      module: {
+          rules: [{
+                  test: /\.css$/i,
+                  //order is important here
+                  use: ['style-loader', 'css-loader'],
+              },
+              {
+                  test: /\.m?js$/,
+                  exclude: /node_modules/,
+                  // We use babel-loader in order to use ES6.
+                  use: {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: ['@babel/preset-env'],
+                          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                      },
+                  },
+              },
+          ],
+      },
   };
 };
